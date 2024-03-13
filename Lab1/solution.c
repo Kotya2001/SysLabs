a31d9b7a95ad6899bc43e09d87de0f6d0aa392b0
ilya Belozerov <iabelozerov@quasarpro.ru>
2024-03-13 23:57:22 +0300

    fix bugs for 1st lab (15 scores) add N for sec param(2)

--- Lab1/solution.c
+++ Lab1/solution.c
@@ -5,9 +5,43 @@
 #include <string.h>
 #include "libcoro.h"
 #include "structs.h"
-#include "mysort.h"
 #include <limits.h>
 #include <time.h>
+#include "assert.h"
+
+
+struct
+array_t
+{
+    int size;
+    int *array;
+};
+
+struct
+filenames_t
+{
+    int count;
+    int index;
+    char **file_names;
+};
+
+
+struct
+my_context
+{
+    int i;
+    char* name;
+    int* total_numbers_count;
+    struct filenames_t *filenames_t;
+    struct array_t **array_t;
+    int sec_start;
+    int nsec_start;
+    int sec_finish;
+    int nsec_finish;
+    int sec_total;
+    int nsec_total;
+
+};
 
 
 int
@@ -28,21 +62,10 @@ create_array(FILE *file, int *array)
     fseek(file, 0, SEEK_SET);
 }
 
-
-struct my_context {
-    int i;
-    char* name;
-    int* total_numbers_count;
-    struct filenames_t *filenames_t;
-    struct array_t **array_t;
-    long long *time_sleep;
-};
-
 static struct my_context *
 my_context_new(int i, char *name, int *total_numbers_count,
                struct filenames_t *filenames_t,
-               struct array_t **array_t,
-               long long *time_sleep)
+               struct array_t **array_t)
 {
     struct my_context *ctx = malloc(sizeof(*ctx));
     ctx->i = i;
@@ -50,8 +73,37 @@ my_context_new(int i, char *name, int *total_numbers_count,
     ctx->filenames_t = filenames_t;
     ctx->total_numbers_count = total_numbers_count;
     ctx->array_t = array_t;
-    ctx->time_sleep = time_sleep;
     return ctx;
+}
+
+static void
+start(struct my_context *ctx)
+{
+    struct timespec time;
+    clock_gettime(CLOCK_MONOTONIC, &time);
+    ctx->sec_start = time.tv_sec;
+    ctx->nsec_start = time.tv_nsec;
+}
+
+static void
+stop(struct my_context *ctx)
+{
+    struct timespec time;
+    clock_gettime(CLOCK_MONOTONIC, &time);
+    ctx->sec_finish = time.tv_sec;
+    ctx->nsec_finish = time.tv_nsec;
+}
+
+static void
+calc_time(struct my_context *ctx)
+{
+    ctx->sec_total += ctx->sec_finish - ctx->sec_start;
+    if (ctx->nsec_finish - ctx->nsec_start < 0) {
+        ctx->nsec_total += 1000000000 + ctx->nsec_finish - ctx->nsec_start;
+        ctx->sec_total--;
+    } else {
+        ctx->nsec_total += ctx->nsec_finish - ctx->nsec_start;
+    }
 }
 
 static void
@@ -59,6 +111,105 @@ my_context_delete(struct my_context *ctx)
 {
     free(ctx->name);
     free(ctx);
+}
+
+
+void *
+my_memcpy(void* dest, const void* src, size_t n)
+{
+    char* char_dest = (char*)dest;
+    const char* char_src = (const char*)src;
+
+    for (size_t i = 0; i < n; i++) {char_dest[i] = char_src[i];}
+
+    return dest;
+}
+
+
+void
+merge(void *array, int left, int center, int right, size_t element_size,
+      int (*comparator)(const void *, const void *))
+{
+
+    int left_size = center - left + 1;
+    int right_size = right - center;
+    int i = 0, j = 0, k = left;
+
+    void* left_arr = malloc(left_size * element_size);
+    void* right_arr = malloc(right_size * element_size);
+
+    assert(left_size);
+    assert(right_size);
+
+    my_memcpy(left_arr, array + (left * element_size), left_size * element_size);
+    my_memcpy(right_arr, array + ((center + 1) * element_size), right_size * element_size);
+
+    while (i < left_size && j < right_size) {
+        if (comparator(left_arr + (i * element_size), right_arr + (j * element_size)) <= 0) {
+            my_memcpy(array + (k * element_size), left_arr + (i * element_size), element_size);
+            i++;
+        } else {
+            my_memcpy(array + (k * element_size), right_arr + (j * element_size), element_size);
+            j++;
+        }
+        k++;
+    }
+
+    while (i < left_size) {
+        my_memcpy(array + (k * element_size), left_arr + (i * element_size), element_size);
+        i++;
+        k++;
+    }
+
+    while (j < right_size) {
+        my_memcpy(array + (k * element_size), right_arr + (j * element_size), element_size);
+        j++;
+        k++;
+    }
+
+    free(left_arr);
+    free(right_arr);
+
+}
+
+
+void
+recursion(void *array, int left, int right, size_t element_size,
+          int (*comparator)(const void *, const void *),
+          struct my_context *ctx)
+{
+    if (left < right) {
+
+        int center = left + (right - left) / 2;
+
+        recursion(array, left, center, element_size, comparator, ctx);
+        recursion(array, center + 1, right, element_size, comparator, ctx);
+
+        merge(array, left, center, right, element_size, comparator);
+
+        stop(ctx);
+        calc_time(ctx);
+        coro_yield();
+        start(ctx);
+
+    }
+
+}
+
+
+int
+mysort(void *array, size_t elements, size_t element_size,
+       int (*comparator)(const void *, const void *),
+       struct my_context *ctx)
+{
+    recursion(array, 0, elements - 1, element_size, comparator, ctx);
+    return 0;
+}
+
+int
+int_cmp(const void *a, const void *b)
+{
+    return *(int*)a - *(int*)b;
 }
 
 static int
@@ -69,13 +220,8 @@ coroutine_mergesort_file(void *context)
     struct my_context *ctx = context;
     int status;
 
-    struct timespec start, end;
-    long long time;
-//    long long time_sleep = 0;
-
+    start(ctx);
     printf("Started coroutine %s\n", ctx->name);
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
 
     while (ctx->filenames_t->index < ctx->filenames_t->count) {
 
@@ -101,22 +247,21 @@ coroutine_mergesort_file(void *context)
         create_array(file, ctx->array_t[index]->array);
         fclose(file);
 
-        mysort(ctx->array_t[index]->array, numbers_count, sizeof(int), int_cmp, ctx->time_sleep);
+        mysort(ctx->array_t[index]->array, numbers_count, sizeof(int), int_cmp, ctx);
+
 
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    time = (end.tv_sec - start.tv_sec) * 1000000LL + (end.tv_nsec - start.tv_nsec) / 1e3;
+    stop(ctx);
+    calc_time(ctx);
 
     long long int switch_count = coro_switch_count(this);
-    printf("%s: количество переключений - %lld, время выполнения: %lld\n", ctx->name, switch_count, llabs(time - *ctx->time_sleep));
-
-    *ctx->time_sleep = 0;
+    printf("%s: количество переключений - %lld, время выполнения: %d\n", ctx->name, switch_count,
+           ctx->sec_total * 1000000  + ctx->nsec_total / 1000);
 
     status = coro_status(this);
 
     my_context_delete(ctx);
-    /* This will be returned from coro_status(). */
     return status;
 }
 
@@ -126,10 +271,7 @@ int
 main(int argc, char **argv)
 {
     struct timespec start, end;
-    long long time;
-    long long *time_sleep = malloc(sizeof(long long ));
-    *time_sleep = 0;
-
+    int time;
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     int n_files = argc - 2;
@@ -145,8 +287,10 @@ main(int argc, char **argv)
     for (int i = 0; i < N; i++) {
         char name[16];
         sprintf(name, "coro_%d", i);
+
         coro_new(coroutine_mergesort_file,
-                 my_context_new(i, name, &total_numbers_count, &filenames_s, array_s, time_sleep));
+                 my_context_new(i, name, &total_numbers_count, &filenames_s, array_s));
+
     }
 
     /* Wait for all the coroutines to end. */
@@ -189,9 +333,6 @@ main(int argc, char **argv)
         }
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    time = (end.tv_sec - start.tv_sec) * 1000000LL + (end.tv_nsec - start.tv_nsec) / 1e3;
-
 
     for (int i = 0; i < n_files; ++i){
         free(array_s[i]->array);
@@ -200,10 +341,12 @@ main(int argc, char **argv)
 
     free(result);
     free(array_s);
-    free(time_sleep);
     fclose(output_file);
 
-    printf("Время выполнения программы: %lld микросекунд\n", time);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+
+    printf("Время выполнения программы: %d микросекунд\n", time);
 
     return 0;
 
--- Lab1/structs.h
+++ Lab1/structs.h
@@ -1,35 +1,51 @@
+////
+//// Created by Илья Белозеров on 11.03.2024.
+////
 //
-// Created by Илья Белозеров on 11.03.2024.
+//#ifndef SYSLABS_UTILS_H
+//#define SYSLABS_UTILS_H
 //
-
-#ifndef SYSLABS_UTILS_H
-#define SYSLABS_UTILS_H
-
-#pragma once
-
-#include <time.h>
-
-struct
-array_t
-{
-    int size;
-    int *array;
-};
-
-struct
-filenames_t
-{
-    int count;
-    int index;
-    char **file_names;
-};
-
+//#pragma once
+//
+//#include <time.h>
+//
+//struct
+//array_t
+//{
+//    int size;
+//    int *array;
+//};
+//
+//struct
+//filenames_t
+//{
+//    int count;
+//    int index;
+//    char **file_names;
+//};
+//
+//struct
+//my_context
+//{
+//    int i;
+//    char* name;
+//    int* total_numbers_count;
+//    struct filenames_t *filenames_t;
+//    struct array_t **array_t;
+//    long long *time_sleep;
+//    struct coroutine_times_t *coroutine_times_t;
+//};
+//
 //struct
 //coroutine_times_t
 //{
-//    struct timespec start_yield, end_yield;
-//    long long time_sleep = 0;
+//    long long sec_start;
+//    long long nsec_start;
+//    long long sec_finish;
+//    long long nsec_finish;
+//    long long sec_total;
+//    long long nsec_total;
 //
 //};
-
-#endif //SYSLABS_UTILS_H
+//
+//#endif //SYSLABS_UTILS_H
